c767c5088252f7970a4f547cad00b1fe22168457
Tomasz Krutowski <tkrutowski@gmail.com>
2021-02-19 11:36:14 +0100

    init

---
+++ src/test/java/net/focik/addresses/AddressesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.focik.addresses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
