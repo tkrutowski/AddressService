8c29826bc02fa4140e298c69c70857738d80186c
Tomasz Krutowski <tkrutowski@gmail.com>
2021-05-23 17:14:36 +0200

    version v1

--- src/main/java/net/focik/addresses/api/AddressController.java
+++ src/main/java/net/focik/addresses/api/AddressController.java
@@ -3,21 +3,15 @@ package net.focik.addresses.api;
 import lombok.AllArgsConstructor;
 import net.focik.addresses.domain.AddressFacade;
 import net.focik.addresses.domain.IAddressDto;
-import net.focik.addresses.domain.dto.TaskCalendarAddressDtoDto;
-import net.focik.addresses.domain.exceptions.AddressDoesNotExistException;
 import net.focik.addresses.domain.share.AddressType;
 import net.focik.addresses.infrastructure.dto.AddressDbDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 @RestController
 @AllArgsConstructor
